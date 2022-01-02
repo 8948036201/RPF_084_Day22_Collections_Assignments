561b1389d13536dfcb2cf52fe77168f5472907cf
8948036201 <azharuddin.ansari108@gmail.com>
2022-01-02 18:46:48 +0530

    Initializing Address and Contact using getters and setters

---
+++ src/com/bridgelabz/collections/Contact.java
@@ -0,0 +1,56 @@
+package com.bridgelabz.collections;
+
+public class Contact {
+
+	private String firstName;
+	private String lastName;
+	private long phoneNumber;
+	private String email;
+	private Address address;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", phoneNumber=" + phoneNumber
+				+ ", email=" + email + ", address=" + address + "]";
+	}
+}
